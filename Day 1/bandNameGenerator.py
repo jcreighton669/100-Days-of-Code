690353e3e6ad2c1a3760354972708cc558d19461
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2022-10-10 15:39:40 -0400

    Day 1 Challenges and Exercises

---
+++ Day 1/bandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Hello and welcome to the Band Name Generator!")
+city = input("What city did you grow up in?\n")
+pet = input("What was the of your pet?\n")
+band_name = city + " " + pet
+
+print("Your band name is: " + band_name)
---
+++ Day 1/printingstart.py
@@ -0,0 +1,4 @@
+# Write your code below this line 👇
+print("Hello world!\nHello world!\nHello world!")
+print("Hello" + " " + "Justin")
+print("Hello " + input("What is your name?") + "!")
