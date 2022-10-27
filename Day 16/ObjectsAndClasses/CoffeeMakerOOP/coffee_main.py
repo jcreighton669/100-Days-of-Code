71c1950a73c6061561edd954d6027cb319f8247b
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2022-10-27 14:34:04 -0400

    Add files via upload

---
+++ Day 16/ObjectsAndClasses/CoffeeMakerOOP/coffee_main.py
@@ -0,0 +1,21 @@
+from coffee_maker import CoffeeMaker
+from menu import Menu
+from money_machine import MoneyMachine
+
+my_coffee_menu = Menu()
+my_coffee_maker = CoffeeMaker()
+my_money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    options = my_coffee_menu.get_items()
+    choice = input(f"What would you like ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        my_coffee_maker.report()
+    else:
+        drink = my_coffee_menu.find_drink(choice)
+        if my_coffee_maker.is_resource_sufficient(drink) and my_money_machine.make_payment(drink.cost):
+            my_coffee_maker.make_coffee(drink)
