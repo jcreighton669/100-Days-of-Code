562192eca2345c696ad4cf526c47393e4c36dc63
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2022-11-01 10:16:21 -0400

    Add files via upload

---
+++ Day 20/snake_game.py
@@ -0,0 +1,8 @@
+# TODO: Control the snake
+# TODO: Populate food on the screen
+# TODO: Detect collision with food
+# TODO: Create a scoreboard
+# TODO: Deal with collision with wall
+# TODO: Deal with collision with tail/body
+
+
