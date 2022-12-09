d058cb3b49c5b59d89aa8f14292b4dd6dcea1c7f
Justin Creighton Personal <jcreighton669@bears.shawu.edu>
2022-12-09 08:00:04 -0500

    Day 29 update

--- Day 29/Project Files/main.py
+++ Day 29/Project Files/main.py
@@ -1,11 +1,64 @@
 from tkinter import *
+from tkinter import messagebox
+import random
 
 # ---------------------------- CONSTANTS ------------------------------- #
 WHITE = "#FFFFFF"
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q',
+               'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    password_input.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_input.get()
+    username = username_input.get()
+    password = password_input.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure that you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
+                                                              f"\nUsername: {username} "
+                                                              f"\nPassword: {password} "
+                                                              f"\nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{website} | {username} | {password}\n")
+                website_input.delete(0, END)
+                password_input.delete(0, END)
+                file.close()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,15 +79,17 @@ password_label.grid(column=0, row=3)
 
 website_input = Entry(width=35)
 website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
 username_input = Entry(width=35)
 username_input.grid(column=1, row=2, columnspan=2)
+username_input.insert(0, "jcreighton669@bears.shawu.edu")
 password_input = Entry(width=21)
 password_input.grid(column=1, row=3)
 
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate)
 generate_password_button.grid(column=2, row=3)
 
-add_info_button = Button(text="Add", width=36)
+add_info_button = Button(text="Add", width=36, command=save)
 add_info_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
