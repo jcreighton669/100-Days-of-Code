ef97c5a4a8c1d4df6cb3fa54e532f550dbd96c05
Justin <jcreight@cisco.com>
2022-12-13 15:51:09 -0500

    Day 30 Begin

--- Day 31/main.py
+++ Day 31/main.py
@@ -1,2 +1,30 @@
+from tkinter import *
+from tkinter import messagebox
+import random
 BACKGROUND_COLOR = "#B1DDC6"
 
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Flashy")
+window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+
+card_front_image = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_image)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0)
+card_back_image = PhotoImage(file="images/card_back.png")
+
+x_mark_image = PhotoImage(file="images/wrong.png")
+check_mark_image = PhotoImage(file="images/right.png")
+
+x_mark_button = Button(image=x_mark_image, highlightthickness=0)
+check_mark_button = Button(image=check_mark_image, highlightthickness=0)
+
+
+x_mark_button.grid(row=1, column=0)
+check_mark_button.grid(row=1, column=1)
+
+
+window.mainloop()
