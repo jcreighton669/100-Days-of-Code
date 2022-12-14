926b8529f115a609ff274f0eb6d103d36e9ad94c
Justin <jcreight@cisco.com>
2022-12-14 10:01:46 -0500

    Day 30 Begin

--- Day 31/main.py
+++ Day 31/main.py
@@ -1,7 +1,11 @@
 from tkinter import *
 from tkinter import messagebox
+import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------------------------- FLASH CARDS ------------------------------- #
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -12,18 +16,20 @@ canvas = Canvas(width=800, height=526)
 
 card_front_image = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_image)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0, columnspan=2)
 card_back_image = PhotoImage(file="images/card_back.png")
 
 x_mark_image = PhotoImage(file="images/wrong.png")
 check_mark_image = PhotoImage(file="images/right.png")
 
-x_mark_button = Button(image=x_mark_image, highlightthickness=0)
+unknown_button = Button(image=x_mark_image, highlightthickness=0)
 check_mark_button = Button(image=check_mark_image, highlightthickness=0)
 
 
-x_mark_button.grid(row=1, column=0)
+unknown_button.grid(row=1, column=0)
 check_mark_button.grid(row=1, column=1)
 
 
