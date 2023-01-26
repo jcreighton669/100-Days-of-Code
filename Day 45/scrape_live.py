d638b4a4cd6373994074f7a60dfabfa794367518
Justin Creighton Personal <jcreighton669@bears.shawu.edu>
2023-01-26 16:25:41 -0500

    Day 45

---
+++ Day 45/scrape_live.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+articles = soup.find_all(name="span", class_="titleline")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.select_one(selector="td span a").get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+most_popular = article_upvotes.index(max(article_upvotes))
+
+print(article_texts[most_popular])
+print(article_links[most_popular])
+print(article_upvotes[most_popular])
+print(most_popular)
