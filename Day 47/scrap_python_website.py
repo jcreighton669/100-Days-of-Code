39a80a53df6fd08af84a5368fc847fa18d31f095
Justin Creighton Personal <jcreighton669@bears.shawu.edu>
2023-01-31 08:36:15 -0500

    Day 47 Start. Updates in Selenium made videos out-of-date. used newer method

--- Day 47/scrap_python_website.py
+++ Day 47/scrap_python_website.py
@@ -5,6 +5,23 @@ service = Service("C:/Development/chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://python.org/")
-events = driver.find_elements(by="class name", value="event-widget")
+
+event_times = driver.find_elements(by="class name", value="event-widget time")
+# for time in event_times:
+#     print(time.text)
+
+event_names = driver.find_elements(by="class name", value="event-widget li a")
+# for event in event_names:
+#     print(event.text)
+
+events = {}
+
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text
+    }
 
 print(events)
+
+driver.quit()
